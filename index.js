17a85cd13f4aa8e829ca6067bdef10a95afdd4e2
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-04-30 20:03:54 +0600

    news data server done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors())
 
@@ -13,6 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news)
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id == id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
